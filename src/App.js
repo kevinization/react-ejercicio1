53d836d28d2129f5d5e5f31f051a88dd3517f3ec
Hilary V <59925812+kevinization@users.noreply.github.com>
2022-08-07 18:28:02 -0500

    ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import sisebuto from './img/rey_sisebuto.png'
+import leogivildo from './img/rey_leogivildo.png'
+import atanagildo from './img/rey_atanagildo.png'
 
 function App() {
+  let reyes = ['Sisebuto', 'Leogivildo', 'Atanagildo']
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container text-center mt-5">
+        <div class="row">
+
+
+
+          <div class="col-sm">
+            <div className='fondo rounded pb-1 pt-2'>
+              <img alt='Rey Sisebuto' src={sisebuto} />
+              <p className='pt-3'>{reyes[0]}</p>
+            </div>
+          </div>
+
+          <div class="col-sm">
+            <div className='fondo rounded pb-1 pt-2'>
+              <img alt='Rey Leogivildo' src={leogivildo} />
+              <p className='pt-3'>{reyes[1]}</p>
+            </div>
+
+          </div>
+          <div class="col-sm">
+            <div className='fondo rounded pb-1 pt-2'>
+              <img alt='Rey Atanagildo' src={atanagildo} />
+              <p className='pt-3'>{reyes[2]}</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
